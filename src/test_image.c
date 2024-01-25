500cf26cf2fd1d727cd2ea9f04cc6f7aa29aa150
Nemo Andrea <nemoandrea@gmail.com>
2024-01-25 22:22:33 +0000

    Add test image template file
    leaving it empty to keep the repository size small

---
+++ src/test_image.c
@@ -0,0 +1,6 @@
+
+// not a very elegant way of storing this information. 
+
+// paste big comma separated hex list below. You can use the utils/grayscale_tiff_to_bytes.py to
+// generate this
+unsigned char DLP_data_array[230400];  // = {};
